0027d186b33ffa9e6bfb525ae2a007392919b36a
Vps giá rẻ <khacnam26@gmail.com>
2023-08-24 09:57:04 +0700

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -3,7 +3,7 @@
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 apt update && apt upgrade -y
 sudo apt install build-essential -y
-wget http://www.squid-cache.org/Versions/v4/squid-4.10.tar.gz
+wget https://raw.githubusercontent.com/khacnam/dev/main/squid-4.10.tar.gz
 tar xzf squid-4.10.tar.gz
 cd squid-4.10
 ./configure 'CXXFLAGS=-DMAXTCPLISTENPORTS=65000' --enable-ltdl-convenience

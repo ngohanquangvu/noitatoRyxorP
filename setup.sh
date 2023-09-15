3557b144f16f373285cc850d005923389150ad4c
Vps giá rẻ <khacnam26@gmail.com>
2023-09-14 20:44:05 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,27 @@
 # Set the PATH to include common command directories
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
+ulimit -n 65536 # increase reaf file limit
+ufw disable # disable Firewall
+
 # Define the range of ports
 FIRST_PORT=1000
 LAST_PORT=2000
 # Set username and password
 USERNAME="onet"
 PASSWORD="onet"
+
+# Get IP addresses
+IP4=$(curl -4 -s icanhazip.com)
+IP6_PREFIX=$(curl -6 -s icanhazip.com | cut -d':' -f1-3)
+# Extract the last part of IPv4
+IP4_LAST_PART=$(echo "$IP4" | awk -F. '{print $4}')
+
+# Calculate STATIC64 using the combination of Current_date and last part of IPv4
+CURRENT_DATE=$(date +"%Y%m%d")
+STATIC64=$(( (CURRENT_DATE + IP4_LAST_PART + 100) % 65536 ))
+STATIC64=$(printf "%04x" "$STATIC64")
+echo "Static 64: ${STATIC64}"
 
 INTERFACE="eth0"
 # Generate hashed password
@@ -16,28 +31,30 @@ HASHED_PASSWORD=$(openssl passwd -apr1 "$PASSWORD")
 # Generate squid.passwords file with hashed password
 echo "$USERNAME:$HASHED_PASSWORD" > /etc/squid/squid.passwords
 
-# Get IP addresses
-IP4=$(curl -4 -s icanhazip.com)
-IP6_PREFIX=$(curl -6 -s icanhazip.com | cut -f1-3 -d':')
+
+
+echo "Removing old configs"
+rm -rf "/etc/squid/acls/outgoing.conf"
+rm -rf "/etc/squid/acls/ports.conf"
 
 # Display IP information
 echo "Internal ip = ${IP4}. Prefix for ip6 = ${IP6_PREFIX}"
-
+echo "Using /64: ${IP6_PREFIX}:${STATIC64}"
 # Define an array of characters for generating IPv6 addresses
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 
 # Define a function to generate an IPv6 address with random segments
-gen48() {
+gen64() {
     ip64() {
         echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
     }
-    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64):$(ip64)"
+    echo "$1:${STATIC64}:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
 # Generate random ipv6
 gen_ipv6() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "$(gen48 $IP6_PREFIX)"
+        echo "$(gen64 $IP6_PREFIX)"
     done
 }
 
@@ -52,7 +69,7 @@ generate_ports_config() {
 generate_acls() {
     for port in $(seq $FIRST_PORT $LAST_PORT); do
         port_var="port$port"
-        echo "acl ${port_var} localport ${port}" >> /etc/squid/outgoing.conf
+        echo "acl ${port_var} localport ${port}"
     done
 }
 
@@ -62,7 +79,7 @@ generate_tcp_outgoing() {
         port_var="port$port"
         ip=$(echo "$IPv6_ADDRESSES" | head -n 1)
         IPv6_ADDRESSES=$(echo "$IPv6_ADDRESSES" | sed -e '1d')
-        echo "tcp_outgoing_address ${ip} ${port_var}" >> /etc/squid/outgoing.conf
+        echo "tcp_outgoing_address ${ip} ${port_var}"
     done
 }
 
@@ -79,6 +96,9 @@ generate_interfaces() {
             fi
         done
     done
+    # Restart Network
+    systemctl restart networking
+    sleep 3
     # Read IPv6 addresses from file
     IPv6_ADDRESSES=$(cat /etc/squid/ipv6add.acl)
 
@@ -90,20 +110,19 @@ generate_interfaces() {
 
 gen_ipv6 >/etc/squid/ipv6add.acl
 
-generate_ports_config > /etc/squid/ports.conf
+generate_ports_config > /etc/squid/acls/ports.conf
 
 # Read IPv6 addresses from file
 IPv6_ADDRESSES=$(cat /etc/squid/ipv6add.acl)
 
 # Generate ACLs and tcp_outgoing_address lines, and save to outgoing.conf
-generate_acls > /etc/squid/outgoing.conf
-generate_tcp_outgoing >> /etc/squid/outgoing.conf
+generate_acls > /etc/squid/acls/outgoing.conf
+generate_tcp_outgoing >> /etc/squid/acls/outgoing.conf
 
 generate_interfaces
 
 # Restart Squid service
-/usr/local/squid/sbin/squid -f /etc/squid/squid.conf
-#systemctl restart squid
+systemctl restart squid
 
 # Set up crontab job to run the entire script every 15 minutes
 # Check if the cron job already exists before adding it
